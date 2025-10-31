0a1cb6d4cb68da2bc3e9420886abca7ba774a638
Joshtri <stuffofyos1516@gmail.com>
2025-10-31 09:16:37 +0700

    Commit: index.tsx

---
+++ components/features/labels/list/index.tsx
@@ -0,0 +1,95 @@
+"use client";
+
+import { useDeleteLabel, useLabels } from "@/services/labelsService";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { Label } from "../interfaces/labels";
+import { ListGrid } from "@/components/ui/ListGrid";
+import { EyeIcon, PencilIcon, PlusIcon } from "lucide-react";
+
+export default function LabelList() {
+  const router = useRouter();
+
+  const { data, isLoading, isError, error } = useLabels();
+  const deleteLabel = useDeleteLabel();
+  const labels = data?.data || [];
+
+  const columns = [
+    {
+      key: "id",
+      label: "ID",
+      value: (label: Label) => label.id,
+    },
+    {
+      key: "name",
+      label: "Name",
+      value: (label: Label) => label.name,
+    },
+    {
+      key: "color",
+      label: "Color",
+      value: (label: Label) => label.color,
+    },
+    {
+      key: "description",
+      label: "Description",
+      value: (label: Label) => label.description,
+    },
+    {
+      key: "actions",
+      label: "Actions",
+      align: "center" as const,
+    },
+  ];
+
+
+  const handleDelete = (id: string) => {
+    deleteLabel.mutateAsync(Number(id)).then(() => {
+      // Router refresh will trigger re-fetch from cache
+      router.refresh();
+    });
+  };
+
+  return (
+    <ListGrid
+      keyField="id"
+      idField="id"
+      title={"Label Management"}
+      description={"Manage your labels"}
+      addButton={{
+        label: "Tambah Label",
+        href: "/labels/create",
+        icon: <PlusIcon className="w-4 h-4" />,
+      }}
+      actionButtons={{
+        show: {
+          label: "View",
+          icon: <EyeIcon className="w-4 h-4" />,
+          onClick: (id) => router.push(`/labels/${id}`),
+        },
+        edit: {
+          label: "Edit",
+          icon: <PencilIcon className="w-4 h-4" />,
+          onClick(id) {
+            router.push(`/labels/${id}/edit`);
+          },
+        },
+        delete: {
+          label: "Delete",
+          onDelete: handleDelete,
+        },
+      }}
+      isError={isError}
+      error={error}
+      loading={isLoading}
+      empty={labels.length === 0}
+      nameField="name"
+      searchPlaceholder="Search labels by name, description, or color..."
+      data={labels}
+      onSearch={(query) => {}}
+      columns={columns as never}
+      pageSize={10}
+      showPagination={true}
+    />
+  );
+}
