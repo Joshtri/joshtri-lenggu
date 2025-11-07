6c3464b571c24f036eca3d28cb70a29c84b80cae
Joshtri <stuffofyos1516@gmail.com>
2025-11-07 18:53:39 +0700

    Commit: 0010_mushy_omega_flight.sql

---
+++ drizzle/0010_mushy_omega_flight.sql
@@ -0,0 +1,3 @@
+ALTER TABLE "posts" ALTER COLUMN "author_id" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "posts" ALTER COLUMN "label_id" SET NOT NULL;--> statement-breakpoint
+ALTER TABLE "posts" ALTER COLUMN "type_id" SET NOT NULL;
