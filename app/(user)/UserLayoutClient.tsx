e81a27cf3d6dd1f51b2d46a24e0d951abe41bfed
Joshtri <stuffofyos1516@gmail.com>
2025-11-10 08:05:58 +0700

    Commit: UserLayoutClient.tsx

--- app/(user)/UserLayoutClient.tsx
+++ app/(user)/UserLayoutClient.tsx
@@ -1,21 +1,24 @@
 "use client";
 
 import { ReactNode } from "react";
+import Navbar from "@/components/layout/navbar";
+import { Footer } from "@/components/layout/footer";
 
 interface UserLayoutClientProps {
   children: ReactNode;
 }
 
 export function UserLayoutClient({ children }: UserLayoutClientProps) {
-  // Simple wrapper - no sidebar, just clean content
-  // The main navbar from app/layout.tsx will be used
+  // Public layout with Navbar and Footer
   return (
-    <div className="min-h-screen bg-gray-50">
-      <main className="py-8">
+    <div className="min-h-screen bg-gray-50 flex flex-col">
+      <Navbar />
+      <main className="flex-1 py-8">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           {children}
         </div>
       </main>
+      <Footer />
     </div>
   );
 }
